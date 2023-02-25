f059a8f9e904bd6cf2fbbc7b3ab188a14d15b201
coreylin <coreylin@tencent.com>
2023-02-25 21:14:25 +0800

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='baselib',
-    version="1.0.3",
+    version="1.0.4",
     description='base common lib for python',
     author='coreylam',
     author_email='coreylam@163.com',
