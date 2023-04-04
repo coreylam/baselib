3a651002c153055daf86e7a869fdea4e3d332c0d
coreylin <coreylin@tencent.com>
2023-04-04 11:36:22 +0800

    feat: 增加企业微信机器人封装

--- baselib/qywx/qywx.py
+++ baselib/qywx/qywx.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 """
+企业微信机器人消息类封装
+- 消息类型：https://developer.work.weixin.qq.com/document/path/90236
+- 加解密方案说明: https://developer.work.weixin.qq.com/devtool/introduce?id=36388
 @author     : coreylin
 @createTime : 2018/12/22
 """
@@ -13,6 +16,20 @@ class QyWechat(object):
     """ 企业微信处理类
     """
     font_tmpl = '<font color="{color}">{info}</font>'
+
+    class EnumPageType:
+        # 按字符分页
+        SPLIT = "split"
+        # 按行分页
+        SPLIT_BY_LINES = "split_by_lines"
+        # 截断
+        CUT = "cut"
+
+    class EnumMsgType:
+        MARKDOWN = "markdown"
+        NEWS = "news"
+        TEXT = "text"
+        IMAGE = "image"
 
     def __init__(
             self,
@@ -69,8 +86,6 @@ class QyWechat(object):
         if mm_list is not None and len(mm_list) > 0:
             data["text"]["mentioned_mobile_list"] = m_list
         return data
-
-
 
     def format_image(self, base64_data, md5):
         data = {
@@ -136,65 +151,85 @@ class QyWechat(object):
         with open(img_path, "rb") as f:
             # b64encode是编码，b64decode是解码
             base64_data = base64.b64encode(f.read())
-        self.send(self.format_image(base64_data, md5))
+        self._send(self.format_image(base64_data, md5))
 
     def send_news(self, data):
         pass
 
-    def send_markdown(self, msg, limit=4096, multi_page=True, post_msg="\n..."):
+    def send_markdown(
+            self, msg, page_type=EnumPageType.CUT, limit=4096, post_msg="\n...",
+            to_user=None, to_party=None, to_tag=None, agent_id=None,
+            enable_duplicate_check=None, duplicate_check_interval=None):
         """ 发送 markdown 格式的数据
-        :param msg: 发送的数据内容（markdown格式）
-        :param limit: 分页参数，每条消息的最大长度（企业微信默认4096），超过该长度报错
-        :param multi_page: 分页参数，消息内容超长的处理方法， True=分页， False=截断
-        :param post_msg: 分页参数，选择截断时，加在消息末尾的补充信息
+        :param string msg: 发送的数据内容（markdown格式）
+        :param int limit: 分页参数，每条消息的最大长度（企业微信默认4096），超过该长度报错
+        :param string page_type: 分页参数，消息内容超长的处理方法， 取值范围：EnumPageType.CUT
+        :param string post_msg: 分页参数，选择截断时，加在消息末尾的补充信息
+        :param list to_user: 指定接收消息的成员，成员ID列表（多个接收者用‘|’分隔，最多支持1000个）
+            特殊情况：指定为"@all"，则向该企业应用的全部成员发送
+        :param list to_party:  指定接收消息的部门，部门ID列表，多个接收者用‘|’分隔，最多支持100个,
+            当touser为"@all"时忽略本参数
+        :param list to_tag: 指定接收消息的标签，标签ID列表，多个接收者用‘|’分隔，最多支持100个。
+            当touser为"@all"时忽略本参数
+        :param int agent_id: 企业应用的id，整型。企业内部开发，可在应用的设置页面查看；第三方服务商，可通过接口 获取企业授权信息 获取该参数值
+        :param int enable_duplicate_check: 表示是否开启重复消息检查，0表示否，1表示是，默认0
+        :param int duplicate_check_interval: 表示是否重复消息检查的时间间隔，默认1800s，最大不超过4小时
         :return:
         """
-        def format_markdown(content, to_user=None, to_party=None,
-                            to_tag=None, agentid=None, enable_duplicate_check=None,
-                            duplicate_check_interval=None):
-            """ 将输入markdown文本转换为企业微信的请求格式。
-            """
-            data = {
-               "msgtype": "markdown",
-               "markdown": {
-                    "content": content
-               }
-            }
-            if to_user is not None:
-                data['touser'] = "|".join(to_user)
-            if to_party is not None:
-                data["toparty"] = " | ".join(to_party)
-            if to_tag is not None:
-                data["totag"] = " | ".join(to_tag)
-            if agentid is not None:
-                data["agentid"] = agentid
-            if enable_duplicate_check is not None:
-                data["enable_duplicate_check"] = enable_duplicate_check
-            if duplicate_check_interval is not None:
-                data["duplicate_check_interval"] = duplicate_check_interval
-            return data
-        if multi_page:
-            msg_list = self.get_page_list(msg, limit)
+        # 根据分页方式，对页面数据进行剪切
+        if page_type.lower() == self.EnumPageType.SPLIT:
+            msg_list = self.split_msg(msg, limit)
+        elif page_type.lower() == self.EnumPageType.SPLIT_BY_LINES:
+            msg_list = self.split_msg_by_line(msg, limit)
         else:
-            msg_list = [self.limit_msg_length(msg, limit, post_msg)]
+            msg_list = [self.cut_msg(msg, limit, post_msg)]
+
         rsp_list = []
         for msg in msg_list:
-            rsp_list.append(self._send(format_markdown(msg)))
+            rsp_list.append(self.raw_send_markdown(
+                msg, to_user, to_party, to_tag, agent_id,
+                enable_duplicate_check, duplicate_check_interval))
         if len(rsp_list) == 1:
             return rsp_list[0]
         return rsp_list
 
+    def raw_send_markdown(
+            self, content, to_user=None, to_party=None, to_tag=None, agent_id=None,
+            enable_duplicate_check=None, duplicate_check_interval=None):
+        """ 发送 markdown 消息
+        """
+        data = {
+            "msgtype": "markdown",
+            "markdown": {
+                "content": content
+            }
+        }
+        if to_user is not None:
+            data['touser'] = "|".join(to_user)
+        if to_party is not None:
+            data["toparty"] = " | ".join(to_party)
+        if to_tag is not None:
+            data["totag"] = " | ".join(to_tag)
+        if agent_id is not None:
+            data["agentid"] = agent_id
+        if enable_duplicate_check is not None:
+            data["enable_duplicate_check"] = enable_duplicate_check
+        if duplicate_check_interval is not None:
+            data["duplicate_check_interval"] = duplicate_check_interval
+        return self._send(data)
+
     def send_text(self, data):
         pass
 
-    def limit_msg_length(self, msg, limit=4096, post_msg="\n..."):
+    def cut_msg(self, msg, limit=4096, post_msg="\n..."):
         """ 将 msg 长度限制在 limit 以内
             - 如果大于 limit， 则截断多余部分，并补充 post_msg
             - 如果小于等于 limit，则直接返回
-        :param msg:
-        :param limit:
-        :param post_msg:
-        :return:
+        :param string msg: 消息内容
+        :param int limit: 消息长度限制
+        :param string post_msg: 截断末尾补充消息
+        :rtype: string
+        :return: 返回截断后数据
         """
         msg_len = len(msg.encode("utf-8"))
         if msg_len <= limit:
@@ -206,30 +241,39 @@ class QyWechat(object):
                 break
         return msg[:-1 - idx] + post_msg
 
-    def split_lines(self, msg, limit=4096):
+    def split_msg(self, msg, limit=4096):
+        """ 将消息 msg 的长度限制（limit），拆分成多条消息(一条消息即一页）
+        :param string msg: 消息内容
+        :param int limit: 消息长度限制
+        :rtype: list
+        :return: 返回分页后的数据列表
+        """
         msg_len = len(msg.encode("utf-8"))
         if msg_len <= limit:
-            return msg
+            return [msg]
         lines = [""]
         line_no = 0
         for char in msg:
-            if len(lines[line_no].encode("utf-8")) <= limit - len(char.encode("utf-8")):
+            if len(lines[line_no].encode("utf-8")) <= limit - \
+                    len(char.encode("utf-8")):
                 lines[line_no] += char
                 continue
             lines.append(char)
             line_no += 1
         return lines
 
-    def get_page_list(self, msg, limit=4096):
+    def split_msg_by_line(self, msg, limit=4096):
         """ 将数据按行分页
+            本函数适用场景： 消息分多行，单行不超过 limit的情况，单行limit会直接截断
+        :param string msg: 消息内容
+        :param int limit: 消息长度限制
+        :rtype: list
         :return: 返回分页后的数据列表
         """
         msg = msg.strip()
         msg_len = len(msg.encode("utf-8"))
         if msg_len < limit:
             return [msg]
-        if len(msg.encode("utf-8"))/len(msg.split("\n")) > limit/10:
-            return self.split_lines(msg)
         page_len = page_no = 0
         page_list = [""]
         for aline in msg.split("\n"):
@@ -240,7 +284,7 @@ class QyWechat(object):
                 continue
             page_len = 0
             page_no += 1
-            page_list.append("")
+            page_list.append(self.cut_msg(aline, limit, "..."))
         return [i.strip() for i in page_list]
 
 
@@ -248,6 +292,8 @@ if __name__ == "__main__":
     def demo():
         key = ""  # robot key
         robot = QyWechat(key=key, is_ssl=True, debug=True)
-        print(robot.send_markdown("hello"))
-
+        aline = "[hello](http://www.baidu.com)"*100
+        print(robot.send_markdown(
+            "# test\n" + aline + "\n" + aline + "\n",
+            page_type=robot.EnumPageType.SPLIT_BY_LINES))
     demo()
