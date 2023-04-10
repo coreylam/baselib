9a604408f38739a021dc2a7cf03acca1e2ddb709
coreylin <coreylin@tencent.com>
2023-04-10 17:55:44 +0800

    release: 发布新版本，增加企业微信机器人消息推送，完善python打包信息

--- packages.sh
+++ packages.sh
@@ -1,3 +1,3 @@
 rm -rf dist
-python setup.py sdist
+python3 setup.py sdist
 twine upload dist/*
--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@
 # from distutils.core import setup
 from setuptools import find_packages
 from setuptools import setup
+from pathlib import Path
 
 setup(
     name='baselib',
-    version="1.0.6",
+    version="1.0.7.1",
     description='base common lib for python',
+    long_description=Path("README.md").read_text(encoding="utf-8"),
+    long_description_content_type="text/markdown",
     author='coreylam',
     author_email='coreylam@163.com',
     url='https://github.com/coreylam/base_common_lib',
@@ -14,5 +17,6 @@ setup(
     include_package_data=True,
     packages=find_packages(),
     zip_safe=False,
-    install_requires=[]
+    install_requires=[],
+    python_requires=">=2.7",
 )
