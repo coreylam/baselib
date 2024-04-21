20fe4b25856113eaa087bf9e69b85f80b47427cf
coreylin <coreylin@tencent.com>
2024-04-21 21:51:10 +0800

    修改打包脚本

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 pkg_ver=`cat version`
 echo $pkg_ver
-sed -i "s/@@@version@@@/${MAGIC_VERSION}/g" setup.py
+sed -i '' "s/@@@version@@@/${pkg_ver}/g" setup.py
 rm -rf dist
 python3 setup.py sdist
 python3 -m twine upload --repository baselib --verbose dist/*
